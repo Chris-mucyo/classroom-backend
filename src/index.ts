827a60e07bb2c5ff1729da0a8cd9af0fe1b83b14
Chris-mucyo <chrisxeventy6@gmail.com>
2026-04-17 10:13:45 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+const PORT = 8000;
+
+
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
